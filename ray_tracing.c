3e0930dff3904218524a76f3ee8060431c7d8104
santhosh selvam <santhosh29.dev@gmail.com>
2026-05-23 14:43:13 +0530

    Clean up includes and fix window initialization
    Removed unnecessary includes and fixed window declaration.

--- ray_tracing.c
+++ ray_tracing.c
@@ -7,12 +7,4 @@ int main(){
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window window* = CreateWindow("Raytracing",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT);
 
-=======
-#include<stdio.h>
-#include<SDL2/SDL.H>
-
-int main(){
-
-    
->>>>>>> 635e94d9e5c7191411cd38474ce6ef54b6f948d9
 }
