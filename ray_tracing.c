6ce2f383ee89c21105d545272ba1819172da3a8b
AtlasBlackwood <santhosh29.dev@gmail.com>
2026-05-23 19:32:13 +0530

    Fill Circle for RayTracing

--- ray_tracing.c
+++ ray_tracing.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
-
+#include <math.h>
 #define WIDTH 900
 #define HEIGHT 600
 #define COLOR_WHITE 0xffffffff
@@ -13,13 +13,18 @@ struct Circle
 };
 
 
-void FillCircle(SDL_Surface* surface,struct Circle circle){
+void FillCircle(SDL_Surface* surface,struct Circle circle,Uint32 color){
+    double radius_squared=pow(circle.radius,2);
     for (double x=circle.x-circle.radius;x<=circle.x+circle.radius;x++){
         for(double y=circle.y-circle.radius;y<=circle.y+circle.radius;y++){
             double distance_squared =pow(x-circle.x,2)+pow(y-circle.y,2);
-        }
+            if(distance_squared<radius_squared){
+                SDL_Rect pixel=(SDL_Rect){x,y,1,1};
+                SDL_FillRect(surface,&pixel,color);
+            }
     }
 
+}
 }
 
 
@@ -28,10 +33,23 @@ int main(int argc, char* argv[]){
     SDL_Window* window=SDL_CreateWindow("RayTracing",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,0);
     SDL_Surface* surface=SDL_GetWindowSurface(window);
     SDL_Rect rect=(SDL_Rect) {200,200,200,200};
-    SDL_FillRect(surface,&rect,COLOR_WHITE);
-    FillCircle(surface,COLOR_WHITE);
+    struct Circle circle={200,200,80};
 
-    SDL_UpdateWindowSurface(window);
-
-    SDL_Delay(5000);
+    int simulation_running=1;
+    SDL_Event event;
+    while (simulation_running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type==SDL_QUIT)
+            {
+                simulation_running=0;
+            }
+            
+        }
+        
+        FillCircle(surface,circle,COLOR_WHITE);
+        SDL_UpdateWindowSurface(window);
+        SDL_Delay(10);
+    }
 }
