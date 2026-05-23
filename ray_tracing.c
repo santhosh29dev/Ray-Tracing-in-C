6725afd4d5a0e2c91b9568189359c02ffe83fe8c
AtlasBlackwood <santhosh29.dev@gmail.com>
2026-05-23 14:40:32 +0530

    Initial commit

---
+++ ray_tracing.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include<SDL2/SDL.h>
+#define WIDTH 900
+#define HEIGHT 600
+int main(){
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window window* = CreateWindow("Raytracing",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT);
+
+}
